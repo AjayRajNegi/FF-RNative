33b0227abffa849658d24317722b03588dfd16c0
Ajay <ajayrajnegi111@gmail.com>
2025-09-14 15:21:36 +0530

    appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PULIC_APPWRITE_CONFIG,
+  platform: "com.redsan.FFCraft",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "68c688b600270d6a4af3",
+  userCollectionId: "user",
+};
